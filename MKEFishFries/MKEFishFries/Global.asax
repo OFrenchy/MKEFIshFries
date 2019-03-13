667e9b1457b944572e1cb786a75158022c0f3ef8
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-13 13:36:20 -0500

    After VS Solution created

---
+++ MKEFishFries/MKEFishFries/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MKEFishFries.MvcApplication" Language="C#" %>
---
+++ MKEFishFries/MKEFishFries/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MKEFishFries.Startup))]
+namespace MKEFishFries
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
