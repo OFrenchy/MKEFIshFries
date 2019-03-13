f4aa0f2fab002bf0ea200808dc197aff2ddd04f3
matthew payne <mdpayne26@yahoo.com>
2019-03-13 16:20:39 -0500

    added Donations model with properties

---
+++ MKEFishFries/MKEFishFries/Models/Donations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MKEFishFries.Models
+{
+    public class Donations
+    {
+        [Key]
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        //[ForeignKey("People")]
+        //public int PesonId { get; set; }
+        //public Person Person { get; set; }
+    }
+}
