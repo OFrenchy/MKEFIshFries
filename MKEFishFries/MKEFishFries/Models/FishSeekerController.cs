fc379a1e6394b51724cea90ded9062fcebfd0304
Robe Gogan <robe.gogan@gmail.com>
2019-03-14 10:17:20 -0500

    FishSeekerController & ParishAdminController added to Controller folder for CRUD functionality available to implement.

---
+++ MKEFishFries/MKEFishFries/Models/FishSeekerController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MKEFishFries.Models
+{
+    public class FishSeekerController : Controller
+    {
+        // GET: FishSeeker
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FishSeeker/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FishSeeker/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FishSeeker/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FishSeeker/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FishSeeker/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FishSeeker/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FishSeeker/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
