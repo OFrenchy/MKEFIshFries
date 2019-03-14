0c330f016533b33d788a4e3097e906e64e0a03dc
Dean Bourgeois <DeanBourgeois@wi.rr.com>
2019-03-14 08:06:48 -0500

    Syncing Pull/Push

--- MKEFishFries/MKEFishFries/Models/People.cs
+++ MKEFishFries/MKEFishFries/Models/People.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MKEFishFries.Models
 {
     public class People
     {
+        [Key]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BusinessName { get; set; }
+        
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
     }
 }
