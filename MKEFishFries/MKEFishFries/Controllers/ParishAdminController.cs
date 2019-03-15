2607bbcf2599a4f25d63632e398aa13e7031bb40
matthew payne <mdpayne26@yahoo.com>
2019-03-15 14:09:19 -0500

    added comments and recieve comments to parish model

--- MKEFishFries/MKEFishFries/Controllers/ParishAdminController.cs
+++ MKEFishFries/MKEFishFries/Controllers/ParishAdminController.cs
@@ -31,6 +31,7 @@ namespace MKEFishFries.Controllers
         // GET: ParishAdmin/Details/5
         public ActionResult Details(int? id)
         {
+            
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
