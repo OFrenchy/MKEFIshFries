98601d89c7fa038dd7d5a5453c76faded5e22a47
Robe Gogan <robe.gogan@gmail.com>
2019-03-17 20:56:30 -0500

    Moved API key from the Index page and removed the business name form from the Create ParishAdmin View.

---
+++ MKEFishFries/MKEFishFries/Models/Access.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MKEFishFries.Models
+{
+    public static class Access
+    {
+        public static string apiKey = "AIzaSyAqPB-xlRlEDxCQcWVRI0pZ9UJCHDhNzaE";
+    }
+}
